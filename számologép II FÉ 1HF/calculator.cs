aec77e69f3aea136920e472761917daae366a302
donaaat <donaaaaat@gmail.com>
2017-02-09 11:16:03 +0100

    Hozzáadtam az összeadás függvényét!

---
+++ számologép II FÉ 1HF/calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace számologép_II_FÉ_1HF
+{
+    class calculator
+    {
+        public static double Osszead(double egyikszam, double masikszam)
+        {
+            return egyikszam + masikszam;
+        }
+    }
+}
